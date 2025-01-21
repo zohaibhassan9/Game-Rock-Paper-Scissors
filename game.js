c495392061bf2299326ce70d109551972cf5654e
zohaibhassan9 <zabaidafashion@gmail.com>
2025-01-21 15:31:51 +0500

    Now the logic in the game are completed. And it have proper functionalties.

--- game.js
+++ game.js
@@ -1,51 +1,7 @@
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissors = document.getElementById("scissors");
-const result = document.getElementById("result");
-
-// Step 1: Define Choices
-// const choices = ['rock', 'paper', 'scissors'];
-
-
-// const resultDisplay = document.getElementById('result');
-// const buttons = document.querySelectorAll('.choices button');
-
-
-// buttons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     const playerChoice = button.id;
-//     const computerChoice = getComputerChoice(); 
-//     const winner = determineWinner(playerChoice, computerChoice);
-//     displayResult(playerChoice, computerChoice, winner);
-//   });
-// });
-
-// function getComputerChoice() {
-//   const randomIndex = Math.floor(Math.random() * choices.length);
-//   return choices[randomIndex];
-// }
-
-// function determineWinner(player, computer) {
-//   if (player === computer) {
-//     return 'It\'s a Tie!';
-//   } else if (
-//     (player === 'rock' && computer === 'scissors') ||
-//     (player === 'paper' && computer === 'rock') ||
-//     (player === 'scissors' && computer === 'paper')
-//   ) {
-//     return 'Player Wins!';
-//   } else {
-//     return 'Computer Wins!';
-//   }
-// }
-
-// function displayResult(player, computer, winner) {
-//   resultDisplay.textContent = `
-//     Player chose: ${player.toUpperCase()}
-//     | Computer chose: ${computer.toUpperCase()}
-//     => ${winner}
-//   `;
-// }
+// const rock = document.getElementById("rock");
+// const paper = document.getElementById("paper");
+// const scissors = document.getElementById("scissors");
+// const result = document.getElementById("result");
 
 
 
@@ -67,7 +23,30 @@ displayResult(playerChoice, computerChoice, winner);
 
 
 function getComputerChoice(){
-const randomIndex = Math.floor(Math.random() * choices.lenght);
+const randomIndex = Math.floor(Math.random() * choices.length);
 return choices[randomIndex];
 }
 
+function determineWinner(player, computer){
+  if (player === computer){
+    return 'It\'s a tie';
+  }
+    else if (
+      (player === 'rock' && computer === 'scissors') ||
+    (player === 'paper' && computer === 'rock') ||
+    (player === 'scissors' && computer === 'paper')
+){
+  return 'player wins';
+}
+else{
+  return 'computer wins';
+}
+}
+
+function displayResult(player, computer, winner){
+  resultDisplay.textContent = `
+  Player Chose: ${player.toUpperCase()}
+  | Computer Chose: ${computer.toUpperCase()}
+  => ${winner}
+  `;
+}
